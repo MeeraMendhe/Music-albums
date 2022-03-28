40470fedbe90934d9b9edb20b6d80e0cff5ffe7a
meera <shu@ahb.com>
2022-03-28 08:06:56 +0530

    add all

--- Frontend/music/src/Components/Sort/Sort.jsx
+++ Frontend/music/src/Components/Sort/Sort.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../Navbar/Navbar'
 import styles from "./Sort.module.css"
+import axios from "axios"
 const Sort = () => {
+    const [data,setData]=useState([])
+    useEffect(()=>
+    {
+     axios({
+  method: 'get',
+  url: `http://localhost:1234/`,
+  headers:{
+      "Content-Type":"application/json"
+  },
+  })
+  .then(function (response) {
+  setData(response.data)
+  });
+    },[])
   return (
     <div>
     <Navbar/>
-      <div className={styles.back}></div>
+      <div className={styles.back}>
+          <button className={styles.btn}>New to old</button>
+          <button className={styles.btn}>
+              Old to new
+          </button>
+          {
+
+          }
+      </div>
     </div>
   )
 }
